1a8b477c2f7793a4444e71bc219346ee909aff69
Manish <manish@dowellresearch.in>
2023-04-09 05:23:22 +0530

    added api url

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 // Axios
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || "https://crud-nfji.onrender.com/";
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
