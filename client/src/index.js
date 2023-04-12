76de69b9d5e11743b8218a895d724d57b9f41847
Manish <manish@dowellresearch.in>
2023-04-12 12:53:23 +0530

    corrected url

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-// Axios
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
@@ -19,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
